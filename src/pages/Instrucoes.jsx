37f6393bcb9ee24bcccad4568f0c6c5a562a3831
verisync5 <verisync5@gmail.com>
2025-08-23 18:10:40 +0000

    mobile instruções feito

--- src/pages/Instrucoes.jsx
+++ src/pages/Instrucoes.jsx
@@ -9,37 +9,38 @@ export function Instrucoes() {
       <main className="max-w-6xl mx-auto pt-24 sm:px-6 lg:px-8 py-12 ">
 
         {/* Title */}
-        <div className="text-center mb-10 pt-24">
-          <h1 className="text-4xl md:text-5xl font-bold mb-6">
+        <div className="text-center md:text-center text-left mb-10 pt-24 md:pt-24 px-6 md:px-0">
+          <h1 className="text-5xl md:text-5xl font-bold mb-6">
             Descubra como o <span className="text-[#01f7ff]">VeriSync</span>
             <br />
             protege a sua casa.
           </h1>
         </div>
 
+
         {/* Texto dentro da caixinha */}
-        <div className="text-center mb-8 pt-8">
+        <div className="text-center mb-8 mt-6 md:mt-8 pt-10">
           <span className="inline-block bg-[#012c5f]/80 text-[#01f7ff] px-6 py-2 rounded-full text-sm font-medium shadow-md">
             ▶ Assista o vídeo abaixo para entender melhor
           </span>
         </div>
 
         {/* Video Placeholder */}
-        <div className="mb-16">
-          <div className="bg-gray-200 rounded-lg w-full max-w-2xl mx-auto aspect-[16/9] flex items-center justify-center relative">
-            <div className="w-12 h-12 bg-red-500 rounded-full flex items-center justify-center">
-              <div className="w-0 h-0 border-l-[10px] border-l-white border-t-[6px] border-t-transparent border-b-[6px] border-b-transparent ml-1"></div>
+        <div className="mb-16 px-4 md:px-0">
+          <div className="bg-gray-200 rounded-lg w-full max-w-md md:max-w-2xl mx-auto aspect-[16/9] flex items-center justify-center relative">
+            <div className="w-10 h-10 md:w-12 md:h-12 bg-red-500 rounded-full flex items-center justify-center">
+              <div className="w-0 h-0 border-l-[8px] md:border-l-[10px] border-l-white border-t-[5px] md:border-t-[6px] border-t-transparent border-b-[5px] md:border-b-[6px] border-b-transparent ml-1"></div>
             </div>
           </div>
         </div>
 
         {/* Steps */}
-        <div className="space-y-16 pt-16">
+        <div className="pt-16 px-6 md:px-0 space-y-20 md:space-y-16">
 
           {/* Passo 1 - Esquerda */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-20 items-start">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-20 items-start">
             <div>
-              <h3 className="text-3xl font-bold text-white mb-6">
+              <h3 className="text-3xl font-bold text-white mb-4 md:mb-6">
                 Passo <span className="text-[#01f7ff]">1</span>:
               </h3>
               <p className="text-base text-gray-300 leading-relaxed">
@@ -51,10 +52,10 @@ export function Instrucoes() {
           </div>
 
           {/* Passo 2 - Direita */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-20 items-start">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-20 items-start">
             <div></div>
             <div>
-              <h3 className="text-3xl font-bold text-white mb-6">
+              <h3 className="text-3xl font-bold text-white mb-4 md:mb-6">
                 Passo <span className="text-[#01f7ff]">2</span>:
               </h3>
               <p className="text-base text-gray-300 leading-relaxed">
@@ -65,9 +66,9 @@ export function Instrucoes() {
           </div>
 
           {/* Passo 3 - Esquerda */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-20  items-start">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-20 items-start">
             <div>
-              <h3 className="text-3xl font-bold text-white mb-6">
+              <h3 className="text-3xl font-bold text-white mb-4 md:mb-6">
                 Passo <span className="text-[#01f7ff]">3</span>:
               </h3>
               <p className="text-base text-gray-300 leading-relaxed">
@@ -79,10 +80,10 @@ export function Instrucoes() {
           </div>
 
           {/* Passo 4 - Direita */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-20  items-start">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-20 items-start">
             <div></div>
             <div>
-              <h3 className="text-3xl font-bold text-white mb-6">
+              <h3 className="text-3xl font-bold text-white mb-4 md:mb-6">
                 Passo <span className="text-[#01f7ff]">4</span>:
               </h3>
               <p className="text-base text-gray-300 leading-relaxed">
@@ -93,24 +94,23 @@ export function Instrucoes() {
           </div>
 
         </div>
+
       </main>
 
       {/* Footer */}
       <div className="pt-16">
-        {/* Footer com altura fixa */}
-        <footer className="px-6 lg:px-12 border-t border-gray-800 h-28 flex items-center">
+        <footer className="px-6 lg:px-12 border-t border-gray-800 py-4 lg:py-0">
           <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center w-full">
 
-            {/* Logo no footer */}
             <div className="flex items-center gap-2">
               <img
                 src={Logo}
                 alt="Logo"
-                className="h-28 w-auto object-contain"
+                className="h-28 lg:h-28 w-auto object-contain"
               />
             </div>
 
-            <p className="text-gray-400 text-sm">
+            <p className="text-gray-400 text-sm mt-4 md:mt-0 text-center md:text-left">
               Todos os direitos reservados © 2025.
             </p>
           </div>
